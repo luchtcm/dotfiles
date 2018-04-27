5d7b0b3267c1169fdc9714b6d727367a195806be
luchtcm <craig@2400baud.com>
2018-04-27 15:10:30 -0500

    initial commit

---
+++ x-config.sh
@@ -0,0 +1,2 @@
+#xrandr --verbose --output eDP1 --auto --output DP2-1 --auto --above eDP1 --output DP2-2 --auto --left-of DP2-1
+xrandr --verbose --output DP2-1 --primary --auto --pos 0x0 --output eDP1 --auto --below DP2-1 --pos 640x3200 --output DP2-2 --auto --right-of DP2-1 --pos 1920x0
